fa8c6718b5bc9cb6f1305cc1f4a2e4b4b50ff662
AkenEagle <akeneagle@gmail.com>
2022-03-22 12:19:20 +0000

    Code refactor

--- src/util/util.js
+++ src/util/util.js
@@ -9,6 +9,21 @@ const sleep = () => {
   });
 };
 
+// Get results from DB
+const getResults = async (query) => {
+  const results = await new Promise((resolve, reject) =>
+    db.query(query, (err, results) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(results);
+      }
+    })
+  );
+
+  return results;
+};
+
 // Declared first question on start
 const askFirstQuestion = async () => {
   const questions = [
@@ -35,17 +50,7 @@ const askFirstQuestion = async () => {
 
 const displayDepartments = async () => {
   const query = "SELECT * FROM department";
-
-  const results = await new Promise((resolve, reject) =>
-    db.query(query, (err, results) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(results);
-      }
-    })
-  );
-  console.log("\n\n");
+  const results = await getResults(query);
   console.table(results);
   await sleep();
 };
@@ -53,17 +58,7 @@ const displayDepartments = async () => {
 const displayRoles = async () => {
   const query =
     "SELECT role.id, role.title, department.name AS department, role.salary FROM role JOIN department ON role.department_id = department.id ORDER BY department.name";
-
-  const results = await new Promise((resolve, reject) =>
-    db.query(query, (err, results) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(results);
-      }
-    })
-  );
-  console.log("\n\n");
+  const results = await getResults(query);
   console.table(results);
   await sleep();
 };
@@ -72,16 +67,7 @@ const displayEmployees = async () => {
   const query =
     "SELECT employee_role.first_name, employee_role.last_name, title , salary,  name AS department,  CONCAT (employee_manager.first_name, ' ', employee_manager.last_name) AS manager FROM employee employee_role LEFT JOIN role ON employee_role.role_id = role.id LEFT JOIN department ON role.department_id = department.id LEFT JOIN employee employee_manager ON employee_role.manager_id = employee_manager.id";
 
-  const results = await new Promise((resolve, reject) =>
-    db.query(query, (err, results) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(results);
-      }
-    })
-  );
-  console.log("\n\n");
+  const results = await getResults(query);
   console.table(results);
   await sleep();
 };
@@ -94,13 +80,9 @@ const addDepartment = async () => {
       message: "Enter department name:",
     },
   ];
-
   const answer = await inquirer.prompt(question);
-
   // execute mysql query
   db.query(`INSERT INTO department (name) VALUES ('${answer.department}')`);
-
-  console.log("\n\n");
   console.log(`Department ${answer.department} succesfully added.`);
   await sleep();
 };
@@ -131,18 +113,6 @@ const addEmployee = () => {
   // execute mysql query
 };
 
-const constructDepartmentChoices = (departments) => {
-  // return an array of department choices
-};
-
-const constructRoleChoices = (roles) => {
-  // return an array of role choices
-};
-
-const constructEmployeeChoices = (roles) => {
-  // return an array of employee choices
-};
-
 module.exports = {
   askFirstQuestion,
   displayDepartments,
@@ -150,8 +120,5 @@ module.exports = {
   displayEmployees,
   addRole,
   addEmployee,
-  constructDepartmentChoices,
-  constructRoleChoices,
-  constructEmployeeChoices,
   addDepartment,
 };
